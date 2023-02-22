dc8af15e934f52e25d75dc79ccf0747d52632cea
bekingalieva2015 <bekingalieva2015@yandex.ru>
2023-02-22 12:15:44 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+	public class Schools
+	{
+		public string Name { get; set; }
+		public string DirectorName { get; set; }
+		public long PhoneNumber { get; set; }
+		public Schools(){
+
+}
+
+		public Schools(string name, string directorName, long phoneNumber)
+		{
+			Name = name;
+			DirectorName = directorName;
+			PhoneNumber = phoneNumber;
+		}
+		public override string ToString()
+		{
+			return "Название образовательной организации \" " + Name + "\". Имя dиректора: " + DirectorName + " Номер телфона директора";
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Classes;
+
+Schools[] scool = new Schools[3];
+
+
+for(int i = 0; i < scool.Length; i++)
+{
+	string name = Console.ReadLine();
+	string directorName = Console.ReadLine();
+	long phoneNumber = Convert.ToInt64(Console.ReadLine());
+	scool[i] = new Schools(name, directorName, phoneNumber);
+}
+Console.WriteLine();
+for(int i = 0;i < scool.Length; i++)
+{
+	Console.WriteLine(scool[i]);
+}
